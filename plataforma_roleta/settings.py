2af7072f508c36ba2a1523ec8f11c637c7763a07
AlexLucena1981 <alexandre.lucena@gmail.com>
2025-07-11 00:59:53 -0300

    Update settings.py

--- plataforma_roleta/settings.py
+++ plataforma_roleta/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+# Lê a variável de ambiente ALLOWED_HOSTS e a transforma em uma lista
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
