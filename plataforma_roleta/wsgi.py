6df4f9d7dd06054a3a02152ac9401f4c4f17f3a7
AlexLucena1981 <alexandre.lucena@gmail.com>
2025-07-11 02:13:51 -0300

    Update wsgi.py

--- plataforma_roleta/wsgi.py
+++ plataforma_roleta/wsgi.py
@@ -1,23 +1,8 @@
 # plataforma_roleta/wsgi.py
+import os
 
-# --- INÍCIO DO CÓDIGO DE TESTE TEMPORÁRIO ---
+from django.core.wsgi import get_wsgi_application
 
-def application(environ, start_response):
-    """Uma aplicação WSGI mínima para teste."""
-    status = '200 OK'
-    headers = [('Content-type', 'text/plain; charset=utf-8')]
-    start_response(status, headers)
-    # Retorna uma mensagem de texto simples
-    return [b"O servidor WSGI esta respondendo corretamente!"]
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plataforma_roleta.settings')
 
-# --- FIM DO CÓDIGO DE TESTE ---
-
-
-# --- CÓDIGO ORIGINAL DO DJANGO (COMENTADO) ---
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plataforma_roleta.settings')
-#
-# application = get_wsgi_application()
+application = get_wsgi_application()
